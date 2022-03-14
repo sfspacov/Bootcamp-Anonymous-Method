838a6d961d2132efe0b7ec37424de33a2e47af08
Stephan Figueredo Spacov <stephan_fs@hotmail.com>
2022-03-14 16:03:39 -0300

    subindo projeto

---
+++ AnonymousMethod/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public delegate void Print(int value);
+
+        public static void PrintMetodo(Print print, int val)
+        {
+            print(val + 10);
+        }
+
+        static void Main(string[] args)
+        {
+            Print print = delegate(int val)
+            {
+                Console.WriteLine("Numero x: " + val);
+            };
+
+            PrintMetodo(print, 10);
+        }
+    }
+}
